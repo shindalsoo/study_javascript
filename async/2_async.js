d2d07919c36bbacfb15f218fd5a187e864ccfe09
목공하는개발자 <40522161+shindalsoo@users.noreply.github.com>
2024-06-22 17:24:17 +0900

    Update 2_async.js

--- async/2_async.js
+++ async/2_async.js
@@ -2,29 +2,28 @@
 
 var async1 = function (param, shindalsoo) {
     shindalsoo(param * 2);
-  };
-  var async2 = function (param, callback) {
+};
+var async2 = function (param, callback) {
     callback(param * 3);
-  };
-  var async3 = function (param, callback) {
+};
+var async3 = function (param, callback) {
     callback(param * 4);
-  };
-  var start = 1;
-  async1(start, (result) => {
+};
+var start = 1;
+async1(start, (result) => {
     console.log(result); // 2
-  });
-  async1(start, (result) => {
+});
+async1(start, (result) => {
     async2(result, (result) => {
       console.log(result); // 6
     });
-  });
-  async1(start, (result) => {
+});
+async1(start, (result) => {
     async2(result, (result) => {
       async3(result, (result) => {
         console.log(result); // 24
       });
     });
-  });
+});
   
-  // 문제는... 함수가 복잡해지면서 들여쓰기에 의해서 코드가 알아보기 힘들어집니다.
-  
+// 문제는... 함수가 복잡해지면서 들여쓰기에 의해서 코드가 알아보기 힘들어집니다.
